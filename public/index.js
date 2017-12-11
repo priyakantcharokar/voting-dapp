acda147ad3f54f7ab849d889f75028188e9acd83
Priyakant Charokar <priyakantcharokar@gmail.com>
2017-12-11 15:10:50 +0530

    Changed for the connect

--- public/index.js
+++ public/index.js
@@ -1,40 +1,67 @@
-$(document).ready(function() {
+
+
+$(document).ready(function () {
   fetch('/candidates')
-  .then(res => res.json())
-  .then(res => {
-    const candidatesTableHTML = res.candidates.map(function(candidate) {
-      return `<tr><td>${candidate.name}</td><td id='${candidate.name}'>${candidate.votes}</td></tr>`;
-    });
-
-    $('#candidatesTable').html(candidatesTableHTML);
-  }).catch(function(err) {
-    // Error :(
-  });
-
-  $('#voteSubmit').click(function(event) {
-    $('#voteSubmit').prop('disabled', true);
-    $("#voteSubmit").removeClass("btn btn-primary").addClass("btn btn-warning");
-    
-    $("#voteSubmit").html('Voting..');
-    const headers = new Headers({
-      "Content-Type": "application/json",
-    });
-
-    fetch('/vote', {
-      method: 'post',
-      headers: headers,
-      body: JSON.stringify({ candidateName: $('#candidateName').val() }),
-    })
     .then(res => res.json())
     .then(res => {
-      $('#' + res.name).html(res.votes);
-      $('#voteSubmit').prop('disabled', false);
-      $("#voteSubmit").removeClass("btn btn-warning").addClass("btn btn-primary");
-      $("#voteSubmit").html('Vote');
-      $('#candidateName').val("");
-    }).catch(function() {
-      // Error
+      console.log(res);
+      $.each(res.candidates, function (index, candidate) {
+        //console.log(candidate.name + "   " + candidate.votes);
+        if ("Rama" === candidate.name) {
+          $('#votesRama').html(candidate.votes);
+        }
+        if ("Phil" === candidate.name) {
+          $('#votesPhil').html(candidate.votes);
+        }
+        if ("Yangli" === candidate.name) {
+          $('#votesYangli').html(candidate.votes);
+        }
+      });
     });
+
+  $("img").click(function () {
+    //alert(this.id);
+    
+    if (this.id == 'buttonRama') {
+      var candidateName = "Rama";
+      processVote(candidateName);
+    }
+    if (this.id == 'buttonPhil') {
+      var candidateName = "Phil";
+      processVote(candidateName);
+    }
+    if (this.id == 'buttonYangli') {
+      var candidateName = "Yangli";
+      processVote(candidateName);
+    }
+   
   });
-});
+})
+
+
+
+
+function processVote(candidateName) {
+  //alert("processVote " + candidateName);
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+  fetch('/vote', {
+    method: 'post',
+    headers: headers,
+    body: JSON.stringify({ candidateName: candidateName }),
+  })
+    .then(res => res.json())
+    .then(res => {
+      if (res.name == "Rama") {
+        $('#votesRama').html(res.votes);
+      }
+      if (res.name == "Phil") {
+        $('#votesPhil').html(res.votes);
+      }
+      if (res.name == "Yangli") {
+        $('#votesYangli').html(res.votes);
+      }
+    })
+}
 
--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
 app.post('/vote', function (req, res) {
   try {
     const candidateName = req.body.candidateName.trim();
-
-    contractInstance.voteForCandidate(candidateName, { from: web3.eth.accounts[0] }, function(result) {
+    //alert("called " + candidateName);
+    contractInstance.voteForCandidate(candidateName, { from: web3.eth.accounts[0] }, function (result) {
       const totalVotes = contractInstance.totalVotesFor.call(candidateName, { from: web3.eth.accounts[0] }).toString();
       res.send({ votes: totalVotes, name: candidateName });
     });
@@ -26,9 +26,9 @@ app.post('/vote', function (req, res) {
   }
 });
 
-app.get('/candidates', function(req, res) {
+app.get('/candidates', function (req, res) {
   try {
-    const candidateVotes = candidates.map(function(candidate) {
+    const candidateVotes = candidates.map(function (candidate) {
       const votes = contractInstance.totalVotesFor.call(candidate, { from: web3.eth.accounts[0] }).toString();
       return {
         name: candidate,
